6fcd842ea7316072d7371ddc0b1ca919ecdaea99
cypher <bart.gniady@gmail.com>
2018-03-14 20:02:19 +0100

    File input via argparser and count_lines DONE

--- Week1/wc.py
+++ Week1/wc.py
@@ -1,2 +1,19 @@
-print("Test")
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="display number of chars, words and letters of given file", type=argparse.FileType("r"))
+args = parser.parse_args()
+
+print(args.file)
+
+
+def count_lines(file):
+    counter = 0
+    for lines in file:
+        counter += 1
+    return counter
+
+def count_chars(file)
+
+
+print(count_lines(args.file))
