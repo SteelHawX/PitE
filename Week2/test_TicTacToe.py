83a7762ebfe0902b7e0d36414d0403a30b409178
Mikołaj Skawiński <>
2018-03-22 21:12:22 +0100

    add testing insert_value and end_players_turn

--- Week2/test_TicTacToe.py
+++ Week2/test_TicTacToe.py
@@ -13,6 +13,35 @@ class TestTicTacToeGame(unittest.TestCase):
         self.assertEqual(self.game.turn, 'X')
 
 
+    def test_insert_value(self):
+        # test player X
+        self.game.insert_value(0, 0)
+        self.assertEqual(self.game.gameState, [['X', ' ', ' '], [' ', ' ', ' '],
+        [' ', ' ', ' ']])
+        self.game.insert_value(2, 2)
+        self.assertEqual(self.game.gameState, [['X', ' ', ' '], [' ', ' ', ' '],
+        [' ', ' ', 'X']])
+
+        # change player to O
+        self.game.turn = 'O'
+
+        # test player O
+        self.game.insert_value(0, 2)
+        self.assertEqual(self.game.gameState, [['X', ' ', 'O'], [' ', ' ', ' '],
+        [' ', ' ', 'X']])
+        self.game.insert_value(2, 0)
+        self.assertEqual(self.game.gameState, [['X', ' ', 'O'], [' ', ' ', ' '],
+        ['O', ' ', 'X']])
+
+    def test_end_players_turn(self):
+        self.game.end_players_turn()
+        self.assertEqual(self.game.turn, 'O')
+        self.game.end_players_turn()
+        self.assertEqual(self.game.turn, 'X')
+
+        self.game.turn = '0'
+        with self.assertRaises(NameError):
+            self.game.end_players_turn()
 
 
 
