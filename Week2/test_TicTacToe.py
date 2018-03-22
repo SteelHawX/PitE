01484c7a6aa4fa794ecb3f3bad70fd310a845004
Mikołaj Skawiński <>
2018-03-22 20:09:10 +0100

    create test module and add setUp and test for init

---
+++ Week2/test_TicTacToe.py
@@ -0,0 +1,21 @@
+import unittest
+from TicTacToe import TicTacToeGame
+from TicTacToe import TicTacToeUI
+
+class TestTicTacToeGame(unittest.TestCase):
+    """docstring forTestTicTacToeGame."""
+    def setUp(self):
+        self.game = TicTacToeGame()
+
+    def test_init(self):
+        self.assertEqual(self.game.gameState, [[' ', ' ', ' '], [' ', ' ', ' '],
+        [' ', ' ', ' ']])
+        self.assertEqual(self.game.turn, 'X')
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
