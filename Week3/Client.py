e630c1742023e356dcd573e8b26941d0f2c36493
Mikołaj Skawiński <>
2018-04-06 06:17:59 +0200

    client side service of game

--- Week3/Client.py
+++ Week3/Client.py
@@ -1,6 +1,6 @@
 import socket
 import pickle
-
+import TicTacToe
 
 class Client:
 
@@ -10,6 +10,8 @@ class Client:
         self.tcp_port = 5005
         self.buffer_size = 1024
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ending_messages = ['Game ends with a draw!', 'You won the game!', 'You lost the game!']
+        self.input_asking_messages = ['Insert index of row (0 to 2)', 'Insert index of column (0 to 2)']
 
     # connects to server
     def connect(self):
@@ -33,3 +35,19 @@ class Client:
     def close(self):
         self.s.close()
 
+if __name__ == '__main__':
+    c = Client()
+    c.connect()
+    c.send('I want play a game!')
+    end = False
+    while not end:
+        mssg = c.receive()
+
+        if mssg in c.input_asking_messages:
+            response = input(mssg)
+            c.send(response)
+        else:
+            print(mssg)
+
+        if mssg in c.ending_messages:
+            end = True
