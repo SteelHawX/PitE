0e5cd762fba577e459bdeebfd3c885df964c4470
Mikołaj Skawiński <>
2018-04-12 14:47:18 +0200

    delete useless client methods

--- Week4/Client/ClientService.py
+++ Week4/Client/ClientService.py
@@ -1,21 +1,5 @@
-
-
-def ask_user_for_row():
-    return input("Insert index of row (0 to 2)")
-
-def ask_user_for_column():
-    return input("Insert index of column (0 to 2)")
-
-def draw_game(board):
-    for row in board:
-        print('---------')
-        line_to_print = ''
-        for value in row:
-            line_to_print += '|'
-            line_to_print += value
-            line_to_print += '|'
-        print(line_to_print)
-        print('---------')
+def ask_user_for_input(question):
+    return input(question)
 
 def show_info(info):
     print(info)
