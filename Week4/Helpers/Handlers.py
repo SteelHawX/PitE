6b5e821a8bd061cb4ecd8799c426d9dc16e4249e
Mikołaj Skawiński <>
2018-04-12 14:48:46 +0200

    change Message handler to fit new header

--- Week4/Helpers/Handlers.py
+++ Week4/Helpers/Handlers.py
@@ -1,33 +1,37 @@
 from Message import Message
-from Message import MessageType
+from Message import StateFlag
 from enum import Enum
 from Wrapper import Wrapper
 import ClientService
 
 class MessageHandler(object):
-    move = (-1, -1)
-    
-    @staticmethod
-    def handle(message):
-        if message.type == MessageType.coordinate_question:
-            MessageHandler.move = (ClientService.ask_user_for_row(), ClientService.ask_user_for_column())
-            # need to send response to server
-            return 1
-        elif message.type == MessageType.game_board:
-            ClientService.draw_game(message.data)
-        elif message.type == MessageType.info:
+    def __init__(self, socket):
+        self.response_data = ""
+        self.socket = socket
+
+
+    def handle(self, message):
+        if message is not Message:
+            print("message is not of type Message")
+            raise TypeError
+
+        if message.header == StateFlag.server_received_valid_response or message.header ==  StateFlag.server_send_info:
             ClientService.show_info(message.data)
-        elif message.type == MessageType.game_ending_result:
+        elif message.header == StateFlag.server_end_connection:
             ClientService.show_info(message.data)
-            # end game
-            return 0
-        
+            #end game
+            return False
+        elif message.header == StateFlag.server_waiting_for_response or message.header == StateFlag.server_received_invalid_response:
+            self.response_data = ClientService.ask_user_for_input(message.data)
+            self.respond_server()
+        elif message.header == StateFlag.server_didnt_receive_response:
+            self.respond_server()
+
+
         #continue game
         return True
-    
-    @staticmethod
-    def respond_server(socket, message):
-        if message.type == MessageType.coordinate_question:
-            data = Wrapper.wrap(MessageHandler.move)
-            socket.send(message)
+
+    def respond_server(self):
+        data = Wrapper.wrap(self.response_data)
+        self.socket.send(data)
 
