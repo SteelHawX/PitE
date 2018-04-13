843ef5cfb89c7f195882bc181eb7fce45b169ff7
Mikołaj Skawiński <>
2018-04-13 17:09:57 +0200

    delete unnecessary code from client

--- Week4/Client/Client.py
+++ Week4/Client/Client.py
@@ -3,10 +3,7 @@ import pickle
 import sys
 sys.path.append("../Games")
 sys.path.append('../Helpers')
-from Message import Message
-from Wrapper import Wrapper
 from Handlers import MessageHandler
-from Message import StateFlag
 
 class Client:
 
@@ -17,23 +14,10 @@ class Client:
         self.buffer_size = 1024
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
     # connects to server
     def connect(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.tcp_ip, self.tcp_port))
-
-    # sends data
-    def send(self, message):
-        self.s.send(message)
-
-    # waits for data send from the server
-    def receive(self):
-        while 1:
-            data = self.s.recv(self.buffer_size)
-            if not data:
-                break
-            return data
 
     # closes socket
     def close(self):
@@ -42,12 +26,10 @@ class Client:
 if __name__ == '__main__':
     c = Client()
     c.connect()
-    mh = MessageHandler(c.s)
+    mh = MessageHandler(c.s, c.buffer_size)
     try:
         while True:
-            byte_server_message = c.receive()
-            server_message = Wrapper.unwrap(byte_server_message)
-            if mh.handle(server_message):
+            if mh.handle():
                 break
     except KeyboardInterrupt:
         c.close()
